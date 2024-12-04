def6ccba9d394505b17754a51b77c22251519b5c
Subhrajit Roy <81874861+SROY9697@users.noreply.github.com>
2024-12-04 17:00:36 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, {  useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts } from "../actions/productActions";
-import { sortProductsByPrice, clearSort } from "../actions/productActions";
+// import { sortProductsByPrice, clearSort } from "../actions/productActions";
 import ProductItem from "./ProductItem";
-import { FaSort, FaTimes } from "react-icons/fa";
+//import { FaSort, FaTimes } from "react-icons/fa";
 
 const ProductList = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products.products);
   const loading = useSelector((state) => state.products.loading);
   const error = useSelector((state) => state.products.error);
-  const [isSorted, setIsSorted] = useState(false);
+ // const [isSorted, setIsSorted] = useState(false);
 
   useEffect(() => {
     if (products.length === 0) {
@@ -18,14 +18,7 @@ const ProductList = () => {
     }
   }, [dispatch, products.length]);
 
-  const handleSortToggle = () => {
-    if (isSorted) {
-      dispatch(clearSort(products));
-    } else {
-      dispatch(sortProductsByPrice());
-    }
-    setIsSorted(!isSorted);
-  };
+
 
   return (
     <div className="product-list">
